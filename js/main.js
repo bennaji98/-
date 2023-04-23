263095c04c9b090fa93b2dbcdb6d9e4eeddeacdb
ben <benyaminnaji98@gmail.com>
2023-04-23 20:57:42 +0430

    add files to github

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    
+    $("#hamburger-menu").click(function () {        
+        $('.nav-list').slideToggle(600);                
+    });
+
+    $(".soal-1").click(function () {
+        $(".javab-1").slideToggle(600);
+    })
+
+    $(".soal-2").click(function () {
+        $(".javab-2").slideToggle(600);
+    })
+
+    $(".soal-3").click(function () {
+        $(".javab-3").slideToggle(600);
+    })
+
+    $(".soal-4").click(function () {
+        $(".javab-4").slideToggle(600);
+    })
+
+    
+    
+})
+
