804e0aa4eedcf323ebb26d7cb1046ee7d9420c28
ben <benyaminnaji98@gmail.com>
2023-05-04 14:35:44 +0430

    animate

--- js/main.js
+++ js/main.js
@@ -7,19 +7,22 @@ $(document).ready(function () {
     $(".soal-1").click(function () {
         
         $(".javab-1").slideToggle(600);
-        $('.svg').toggleClass('rotate');
+        $('.soal-1 svg').toggleClass('rotate');
     })
 
     $(".soal-2").click(function () {
         $(".javab-2").slideToggle(600);
+        $('.soal-2 svg').toggleClass('rotate');
     })
 
     $(".soal-3").click(function () {
         $(".javab-3").slideToggle(600);
+        $('.soal-3 svg').toggleClass('rotate');
     })
 
     $(".soal-4").click(function () {
         $(".javab-4").slideToggle(600);
+        $('.soal-4 svg').toggleClass('rotate');
     })
 
 
